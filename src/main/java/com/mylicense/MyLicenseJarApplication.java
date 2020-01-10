30aa71d5f08d2689f037e474177417e753e1ce2d
xiaojieWoody <1093520060@qq.com>
2020-01-10 11:34:15 +0800

    init

---
+++ src/main/java/com/mylicense/MyLicenseJarApplication.java
@@ -0,0 +1,13 @@
+package com.mylicense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLicenseJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLicenseJarApplication.class, args);
+	}
+
+}
